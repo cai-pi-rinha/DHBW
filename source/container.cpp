f9578d2848eb6f4905772d8f9aa5de57030c66b6
Benedikt Hell <benedikt.hell95@gmail.com>
2019-02-10 12:16:56 +0100

    container: basic class with empty methods added

---
+++ source/container.cpp
@@ -0,0 +1,13 @@
+#include "container.hpp"
+ 
+// constructor
+Container::Container(void)
+{
+    m_length = 0;
+}
+ 
+// Date member function
+int Container::getlength(void)
+{
+    return: m_length;
+}
