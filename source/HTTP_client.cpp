80cd94e45413be7b6ecb65cab582cef338239d97
Benedikt Hell <benedikt.hell95@gmail.com>
2019-03-16 15:33:29 +0100

    HTTP_client.cpp: constructors and destructor added

--- source/HTTP_client.cpp
+++ source/HTTP_client.cpp
@@ -1,14 +1,21 @@
 #include "HTTP_client.hpp"
 #include "comm_processor.hpp"
 
-HTTP_client::HTTP_client()
+HTTP_client::HTTP_client(int buffer_size)
+    : TCP_client(buffer_size)
+{
+    comm_proc = new http_processor(this);
+}
+
+HTTP_client::HTTP_client(const char* ip, const char* port, int buffer_size)
+    : TCP_client(ip, port, buffer_size)
 {
     comm_proc = new http_processor(this);
 }
 
 HTTP_client::~HTTP_client()
 {
-    //dtor
+    terminate_connection();
 }
 
 HTTP_client::HTTP_client(const HTTP_client& other)
