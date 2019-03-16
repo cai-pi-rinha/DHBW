439bebccdedefec29ce8483201e0b63f44d895ff
Benedikt Hell <benedikt.hell95@gmail.com>
2019-03-16 17:12:28 +0100

    main: #if directives for server/client added

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@
 #include "HTTP_server.hpp"
 #include "HTTP_client.hpp"
 
+#define SERVER_1_CLIENT_0 0
+
 using namespace std;
 
 int main()
 {
-/*
+
+#if SERVER_1_CLIENT_0 == 1
+    cout << "server started" << endl;
     HTTP_server mein_server("127.0.0.1","55554", 64);
     while(1)
     {
@@ -19,16 +23,22 @@ int main()
         mein_server.process();
         cout << "received information: " << mein_server.get_http_header().to_String() << endl;
         mein_server.send_response_and_terminate_conn();
-    }*/
+    }
 
-
+#elif SERVER_1_CLIENT_0 == 0
+    cout << "client started" << endl;
     String demo_string = "hello world";
     HTTP_client mein_client("127.0.0.1","55554", 64);
     mein_client.start_client();
+    cout << "started" << endl;
     mein_client.send(&demo_string);
+    cout << "sent" << endl;
     mein_client.receive();
+    cout << "received" << endl;
     cout << "received information: " << mein_client.get_http_header().to_String() << endl;
     mein_client.terminate_connection();
+
+#endif // SERVER_CLIENT
 
     return 0;
 }
